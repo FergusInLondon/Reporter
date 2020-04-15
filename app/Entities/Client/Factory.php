1e428f167bbe3e5b50686a7c420d29395c276fe6
Fergus <fergus@fergus.london>
2020-04-15 09:57:52 +0100

    [ entities ] factory scaffolding

---
+++ app/Entities/Client/Factory.php
@@ -0,0 +1,9 @@
+<?php namespace App\Entities\Client;
+
+use Faker\Generator as Faker;
+
+class Factory {
+    public static generate(Faker $faker) {
+        return []
+    }
+}
---
+++ app/Entities/File/Factory.php
@@ -0,0 +1,9 @@
+<?php namespace App\Entities\File;
+
+use Faker\Generator as Faker;
+
+class Factory {
+    public static generate(Faker $faker) {
+        return []
+    }
+}
